b71571475316fceb811cc2fc1c73cc5fc541aa36
Bryce Richardson <drichardson2@harding.edu>
2021-11-19 14:28:57 -0600

    finished

---
+++ src/hooks/useFolder.js
@@ -0,0 +1,111 @@
+import { useReducer, useEffect } from 'react';
+import { database } from '../firebase';
+import { useAuth } from '../contexts/AuthContext';
+
+//happens anytime you select a folder
+const ACTIONS = {
+  SELECT_FOLDER: 'select-folder',
+  UPDATE_FOLDER: 'update-folder',
+  SET_CHILD_FOLDERS: 'set-child-folders',
+  SET_CHILD_FILES: 'set-child-files',
+};
+
+// Base layer of my app
+export const ROOT_FOLDER = { name: 'Root', id: null, path: [] };
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.SELECT_FOLDER:
+      return {
+        folderId: payload.folderId,
+        folder: payload.folder,
+        childFiles: [],
+        childFolders: [],
+      };
+
+    case ACTIONS.UPDATE_FOLDER:
+      return {
+        //update the folder portion of our state
+        ...state,
+        folder: payload.folder,
+      };
+    case ACTIONS.SET_CHILD_FOLDERS:
+      return {
+        //update the folder portion of our state
+        ...state,
+        childFolders: payload.folder,
+      };
+    case ACTIONS.SET_CHILD_FILES:
+      return {
+        //update the folder portion of our state
+        ...state,
+        childFiles: payload.childFiles,
+      };
+    default:
+      return state;
+  }
+}
+
+export function useFolder(folderId = null, folder = null) {
+  const [state, dispatch] = useReducer(reducer, {
+    folderId,
+    folder,
+    childFolders: [],
+    childFiles: [],
+  });
+  const { currentUser } = useAuth();
+
+  //anytime that happens it will reset to the above code
+  useEffect(() => {
+    dispatch({ type: ACTIONS.SELECT_FOLDER, payload: { folderId, folder } });
+  }, [folderId, folder]);
+
+  // Root Folder when no other folder
+  useEffect(() => {
+    if (folderId == null) {
+      return dispatch({
+        type: ACTIONS.UPDATE_FOLDER,
+        payload: { folder: ROOT_FOLDER },
+      });
+    }
+
+    database.folders
+      .doc(folderId)
+      .get()
+      .then((doc) => {
+        dispatch({
+          type: ACTIONS.UPDATE_FOLDER,
+          payload: { folder: database.formatDoc(doc) },
+        });
+      })
+      .catch(() => {
+        //if error update to root folder
+        dispatch({
+          type: ACTIONS.UPDATE_FOLDER,
+          payload: { folder: ROOT_FOLDER },
+        });
+      });
+  }, [folderId]);
+
+  useEffect(
+    () => {
+      //Firebase (property looking for, check, value checked against property)
+      return (
+        database.files
+          .where('folderId', '==', folderId)
+          .where('userId', '==', currentUser.uid)
+          // .orderBy('createAt')
+          .onSnapshot((snapshot) => {
+            dispatch({
+              type: ACTIONS.SET_CHILD_FILES,
+              payload: { childFiles: snapshot.docs.map(database.formatDoc) },
+            });
+          })
+      );
+    },
+    //reset on change
+    [folderId, currentUser]
+  );
+
+  return state;
+}
